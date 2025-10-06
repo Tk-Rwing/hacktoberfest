f108f2bc5e3f154d87f414cd28899462605745c3
Tk-Rwing <72181141+Tk-Rwing@users.noreply.github.com>
2025-10-06 12:01:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5,6,7,8,9,4,5,3,5,7,8,1,2];
+const fruits = ['apple','banana','cherry','date'];
+
+let text = "Hello JavaScript, Welcome to our wolrd best JavaScript course";
+
+let vowels = 'aeiou';
+
+
+console.log(Math.floor(4.9));
