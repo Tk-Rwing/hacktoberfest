5fd321562f9b9ce8c801f76fcde3c057bd37b5f2
tktecno <68456825+tktecno@users.noreply.github.com>
2025-10-06 12:13:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const numbers = [1,2,3,4,5,6,7,8,9,4,5,3,5,7,8,1,2];
 const fruits = ['apple','banana','cherry','date'];
 
-let text = "Hello JavaScript, Welcome to our wolrd best JavaScript course";
+let text = "Hello JavaScript, Welcome to ourt JavaScript course";
 
 let vowels = 'aeiou';
 
 
 console.log(Math.floor(4.9));
+
